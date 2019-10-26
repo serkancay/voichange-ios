baf15137cce96b2b32ff94be9f49dfd610f331f2
S.Serkan Çay <sserkancay@gmail.com>
2019-10-26 22:01:34 +0300

    Add button actions on the recording user interface

--- Voichange/ViewController.swift
+++ Voichange/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordingButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    @IBOutlet weak var recordingInfoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
 
-
+    @IBAction func startRecording(_ sender: Any) {
+        print("Start recording")
+        recordingButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+        recordingInfoLabel.text = "Recording voice.."
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording")
+        recordingButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingInfoLabel.text = "Tap to record"
+    }
+    
 }
 
