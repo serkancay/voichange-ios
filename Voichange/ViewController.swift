67e56ae8780912151a4d985578018d97a71d59f8
S.Serkan Çay <sserkancay@gmail.com>
2019-10-26 18:06:30 +0300

    Initialize project

---
+++ Voichange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voichange
+//
+//  Created by S.Serkan Çay on 26.10.2019.
+//  Copyright © 2019 S.Serkan Çay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
